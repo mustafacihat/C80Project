696c95dba6e54bc6988e578cbc5bbfcbe9c9f47a
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-01-31 21:13:28 +0100

    updated with second commit Cocomelon.java

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -2,5 +2,6 @@ public class Cocomelon {
 
     public static void main(String[] args) {
         System.out.println("this is for first commit");
+        System.out.println("this is for second commit");
     }
 }
