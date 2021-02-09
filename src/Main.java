626b162147814ff4f9ee0e2bef37f5d24dd3818e
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-02-09 23:34:48 +0100

    updated Apple.java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,13 @@ public class Main {
         //everything seems fine
         //try to be successful
         //lets finish the job
+
+
+
+        // write your code here
+        System.out.println("Hello git last day");
+        //everything seems fine
+        //try to be successful
+        //lets finish the job
     }
 }
