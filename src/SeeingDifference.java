68965969c9d88ef9ade1cb7fb407f55aee8d1a78
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-02-09 23:51:56 +0100

    last commit for hour5 branch

--- src/SeeingDifference.java
+++ src/SeeingDifference.java
@@ -2,5 +2,7 @@ public class SeeingDifference {
 
     public static void main(String[] args) {
         System.out.println("we added some codes just for seeing the differences");
+        System.out.println("we added some extra codes");
+        System.out.println("this is the codes four hour 5");
     }
 }
