8b085b461649ecb64cde0cd2e7fe1cfd59104fdd
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-02-09 23:52:41 +0100

    last commit for master branch

--- src/SeeingDifference.java
+++ src/SeeingDifference.java
@@ -2,5 +2,7 @@ public class SeeingDifference {
 
     public static void main(String[] args) {
         System.out.println("we added some codes just for seeing the differences");
+        System.out.println("this is the last commit for master branch");
+        System.out.println("yes last commit");
     }
 }
