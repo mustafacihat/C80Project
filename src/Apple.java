5d2f5a8e6a8e085c73e7c1428b0a30624880344c
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-02-09 23:16:38 +0100

    added

--- src/Apple.java
+++ src/Apple.java
@@ -2,4 +2,11 @@ public class Apple {
     //are we there yet
     //hello hello
     //Class pet, jely bean
+    //hello
+    //do it//
+    //we love the sea
+    //mimitate
+    public static void main(String[] args) {
+        System.out.println("I want to find merge conflict example");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello git last day");
-        //everything seems fine
+        //et seems fine here
         //try to be successful
         //lets finish the job
 
