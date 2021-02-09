ef376668355fb40b5b68d93f1c7ed46a73735d38
Mustafa Cihat YAPICI <57036952+mustafacihat@users.noreply.github.com>
2021-02-10 00:07:43 +0100

    updated SeeingDifference.java

--- src/SeeingDifference.java
+++ src/SeeingDifference.java
@@ -3,7 +3,7 @@ public class SeeingDifference {
     public static void main(String[] args) {
         System.out.println("we added some codes just for seeing the differences");
         System.out.println("this is the last commit for master branch");
-        System.out.println("yes last commit");
+       
         System.out.println("we added some extra codes");
         System.out.println("this is the codes four hour 5");
 
