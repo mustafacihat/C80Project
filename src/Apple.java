5001e682976657c43f14b31f1b84712bf7304339
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-02-09 23:24:42 +0100

    updated Apple.java

--- src/Apple.java
+++ src/Apple.java
@@ -1,5 +1,12 @@
 public class Apple {
     //are we there yet
     //hello hello
+
+    public static void main(String[] args) {
+
+        //some
+        System.out.println("some codes here" );
+        System.out.println("good night");
+    }
     //Class pet, jely bean
 }
